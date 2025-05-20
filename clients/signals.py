0fef47766d8fc19cfe8bfe435e9781f9a5046581
yosephdev <yosephbet@gmail.com>
2025-05-20 15:59:31 +0200

    feat(signals): Add auto profile creation for clients

---
+++ clients/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from .models import ClientProfile
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_client_profile(sender, instance, created, **kwargs):
+    if created and instance.is_client:
+        ClientProfile.objects.get_or_create(user=instance)
