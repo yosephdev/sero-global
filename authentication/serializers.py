6ee6a82ad60a8443381668fe40d3609cb9971354
yosephdev <yosephbet@gmail.com>
2025-05-20 18:49:57 +0200

    feat(auth): Add RegisterSerializer and update UserSerializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,26 +1,51 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'password', 'is_therapist', 'is_client')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password', 'role', 'first_name', 'last_name')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'role': {'read_only': True}
+        }
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
 
+class RegisterSerializer(UserSerializer):
+    password_confirm = serializers.CharField(write_only=True, required=True)
+    
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ('password_confirm',)
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': True},
+            'email': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs.pop('password_confirm'):
+            raise serializers.ValidationError("Passwords do not match")
+        return attrs
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
-        token = super().get_token(user)       
+        token = super().get_token(user)
+        token['role'] = user.role
         token['email'] = user.email
-        token['first_name'] = user.first_name
-        token['last_name'] = user.last_name        
-        token['is_therapist'] = user.is_therapist
-        token['is_client'] = user.is_client
         return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['user'] = UserSerializer(self.user).data
+        return data
