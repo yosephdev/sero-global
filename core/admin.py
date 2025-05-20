117573d6fba43cc6360463d3c35d589bf0df84e7
yosephdev <yosephbet@gmail.com>
2025-05-20 18:49:48 +0200

    refactor(admin): Update UserAdmin for role-based auth

--- core/admin.py
+++ core/admin.py
@@ -1,30 +1,31 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import User
 
 # Register your models here.
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     model = User
-    list_display = ('email', 'username', 'is_active', 'is_staff', 'is_superuser', 'is_therapist', 'is_client')
-    list_filter = ('is_active', 'is_staff', 'is_superuser', 'is_therapist', 'is_client')
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active')
     fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'date_of_birth', 'profile_picture', 'bio')}),
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number')}),
         (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_therapist', 'is_client', 'groups', 'user_permissions'),
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Role'), {'fields': ('role',)}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2'),
+            'fields': ('email', 'password1', 'password2', 'role'),
         }),
     )
-    search_fields = ('email', 'username', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
-# Register the custom user model
 admin.site.register(User, CustomUserAdmin)
