7967aebad341a41709a8b5ad528d14f1e8a58dc7
yosephdev <yosephbet@gmail.com>
2025-05-20 18:50:12 +0200

    feat(auth): Add auth endpoints for registration and profile

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView
+from .views import RegisterView, CustomTokenObtainPairView, UserProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
 ]
