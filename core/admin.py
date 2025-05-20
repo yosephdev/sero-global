8d0dcaa271b245296e0cb76e225be3ee3b59e4ac
yosephdev <yosephbet@gmail.com>
2025-05-20 14:58:05 +0200

    feat: Add custom User model and admin configuration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'username', 'is_active', 'is_staff', 'is_superuser', 'is_therapist', 'is_client')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'is_therapist', 'is_client')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'date_of_birth', 'profile_picture', 'bio')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_therapist', 'is_client', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
+
+# Register the custom user model
+admin.site.register(User, CustomUserAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
 # Create your models here.
+class User(AbstractUser):
+    """
+    Custom user model for Sero Global.
+    """
+    email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField(_('phone number'), max_length=20, blank=True)
+    is_therapist = models.BooleanField(_('is therapist'), default=False)
+    is_client = models.BooleanField(_('is client'), default=False)
+    date_of_birth = models.DateField(_('date of birth'), null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    bio = models.TextField(_('bio'), blank=True)
+
+    objects = CustomUserManager()
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+    
+    
+  
+    
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        ordering = ['-date_joined']
+
+    def __str__(self):
+        return self.email
