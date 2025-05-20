c8cf60f2fed351cef089e29ae1d52b928d8a445d
yosephdev <yosephbet@gmail.com>
2025-05-20 18:50:03 +0200

    feat(auth): Add RegisterView and UserProfileView

--- authentication/views.py
+++ authentication/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserSerializer, CustomTokenObtainPairSerializer
+from .serializers import RegisterSerializer, UserSerializer, CustomTokenObtainPairSerializer
+from core.models import User
 
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        user = serializer.save()
         headers = self.get_success_headers(serializer.data)
         return Response(
-            {"message": "User registered successfully"},
+            {"detail": "User registered successfully"},
             status=status.HTTP_201_CREATED,
             headers=headers
         )
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
