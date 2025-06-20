53f7ff13d74927fb26ed2af31a6e1d0303632ab6
Yoseph <yosephbet@gmail.com>
2025-06-20 08:39:06 +0200

    security: fix configuration and protect sensitive data

--- sero_global/settings.py
+++ sero_global/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-production')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
