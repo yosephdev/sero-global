7952d716e5a7d1180dc0c19d77869a0bc608d08f
Yoseph <yosephbet@gmail.com>
2025-06-20 08:21:28 +0200

    feat: enhance therapist app functionality

--- therapists/urls.py
+++ therapists/urls.py
@@ -1,7 +1,11 @@
-# therapists/urls.py
 from django.urls import path
-from .views import TherapistProfileView
+from . import views
+
+app_name = 'therapists'
 
 urlpatterns = [
-    path('profile/', TherapistProfileView.as_view(), name='therapist-profile'),
+    path('', views.TherapistProfileView.as_view(), name='list'),
+    path('<int:pk>/', views.TherapistProfileView.as_view(), name='detail'),
+    path('profile/', views.TherapistProfileView.as_view(), name='profile'),
+    path('profile/edit/', views.TherapistProfileView.as_view(), name='profile_edit'),
 ]
--- therapists/views.py
+++ therapists/views.py
@@ -1,22 +1,97 @@
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, CreateView, UpdateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
+from rest_framework import generics, permissions
 from .models import TherapistProfile
+from .forms import TherapistProfileForm
 from .serializers import TherapistProfileSerializer
 
-class TherapistProfileView(generics.RetrieveUpdateAPIView):
-    serializer_class = TherapistProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self):        
+# Frontend Views
+class TherapistListView(ListView):
+    model = TherapistProfile
+    template_name = 'therapists/list.html'
+    context_object_name = 'therapists'
+    paginate_by = 12
+    
+    def get_queryset(self):
+        return TherapistProfile.objects.filter(is_available=True)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Find a Therapist'
+        return context
+
+
+class TherapistDetailView(DetailView):
+    model = TherapistProfile
+    template_name = 'therapists/detail.html'
+    context_object_name = 'therapist'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Dr. {self.object.user.get_full_name()}'
+        return context
+
+
+class TherapistProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'therapists/profile.html'
+    login_url = reverse_lazy('core:login')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'My Profile'
+        
+        # Get or create therapist profile
+        profile, created = TherapistProfile.objects.get_or_create(
+            user=self.request.user
+        )
+        context['profile'] = profile
+        return context
+
+
+class TherapistProfileEditView(LoginRequiredMixin, UpdateView):
+    model = TherapistProfile
+    form_class = TherapistProfileForm
+    template_name = 'therapists/profile_edit.html'
+    success_url = reverse_lazy('therapists:profile')
+    login_url = reverse_lazy('core:login')
+    
+    def get_object(self):
         profile, created = TherapistProfile.objects.get_or_create(
             user=self.request.user
         )
         return profile
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Your profile has been updated successfully!')
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit Profile'
+        return context
 
-    def get(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+
+# API Views
+class TherapistProfileListCreateAPIView(generics.ListCreateAPIView):
+    queryset = TherapistProfile.objects.all()
+    serializer_class = TherapistProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TherapistProfileRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TherapistProfile.objects.all()
+    serializer_class = TherapistProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return get_object_or_404(TherapistProfile, user=self.request.user)
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
