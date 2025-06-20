c6daffcd307b71bf9116cd9773cce132fd6b0900
Yoseph <yosephbet@gmail.com>
2025-06-20 12:25:57 +0200

    fix: add Heroku domain to ALLOWED_HOSTS

--- sero_global/settings.py
+++ sero_global/settings.py
@@ -11,7 +11,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'sero-global-3fbd7256cc0a.herokuapp.com',
+    '.herokuapp.com',
+]
 
 # Application definition
 DJANGO_APPS = [
